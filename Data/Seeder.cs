15491ffd7b407af3611298133bd440ac6503d32f
David <david.ajigbotoluwa@expertflow.com>
2023-02-20 05:04:42 +0100

    changed line

--- Data/Seeder.cs
+++ Data/Seeder.cs
@@ -5,7 +5,7 @@ namespace WebApiEntityFrameworkDockerSqlServer.Data
     public static class Seeder
     {
         
-        // This is purely for a demo, don't anything like this in a real application!
+        //This is purely for a demo, don't anything like this in a real application!
         public static void Seed(this SalesContext salesContext)
         {
             if (!salesContext.Products.Any())
